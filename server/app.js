cf526a0511d355745b6950bffc170c80eee2bbdc
iburlakov <iburlakov@gmail.com>
2020-04-01 15:18:40 +0300

    Cleaned up node server code.

--- server/app.js
+++ server/app.js
@@ -1,84 +1,33 @@
 const express = require('express');
 const path = require('path')
-//const bodyParser = require('body-parser');
 
-const config = require('./config');
-//const storage = require('./storage');
-//const client = require('./syncpClient');
-const {handleError} = require('./common');
-
+const axios = require('axios');
 var cors = require('cors')
-//var app = express()
 const qs = require('qs');
 
+const config = require('./config');
 
+const authApiClient = axios.create({
+    baseURL: 'https://api.syncplicity.com/'
+});
 
 app = express();
 
-
-app.use(express.static('front'));
-
+// TODO: remove this in production environment
 app.use(cors());
 
 app.use(express.json());
 
-// host react app in express
-app.use(express.static(path.join(__dirname, '../build')));
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+app.get('/api/noop', (req, res)=> {
+    res.status("200").send('NoOp');
 });
-
-app.get('/noop', (req, res)=> {
-    console.log(process.env.NODE_ENV);
-    res.status("200").send('OK');
-});
-
-const axios = require('axios');
-const client = axios.create({
-    baseURL: 'https://api.syncplicity.com/'
-});
-
 
 app.post('/api/auth', (req, res) => {
-    const code =  req.body.code;
-    console.log(req.body);
-    console.log(req.body.code);
-    console.log(config.appId);
-    console.log(config.appSecret);
+    const code = req.body.code;
 
-    auth(code, config.appId, config.appSecret)
-        .then(token => {
-            //storage[token.user_email] = token;
-            console.log('got token' + token);
-            res.send(token).status(200);
-        })
-        .catch(err => {
+    const appIdPlusAppSecret =  Buffer.from(`${config.appId}:${config.appSecret}`).toString('base64');
 
-            console.log(err.message);
-            res.status(500).send(err.message);
-        });
-});
-
-app.get('api/refresh/:email', (req, res) => {
-    // TODO: store refresh token in strorage
-    res.status(501);
- });
-
- // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
-
-function handleErrorI(err) {
-    console.log(`ERROR ${err.response.config.method} ${err.response.config.url} -> ${err.response.status}:${err.response.statusText} -> ${JSON.stringify(err.response.data)}`);
-    
-    throw err;
-}
-
-function auth(code, clientId, clientSecret) {
-    const authToken =  Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
-
-    return client.post('oauth/token',
+    authApiClient.post('oauth/token',
         qs.stringify({ 
             grant_type:'authorization_code',
             response_type:'code',
@@ -87,17 +36,27 @@ function auth(code, clientId, clientSecret) {
         {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                "Authorization": `Basic ${authToken}`
+                "Authorization": `Basic ${appIdPlusAppSecret}`
             }
         })
         .then(response => response.data)
-        .catch(handleErrorI);
-}
+        .then(token => {
+            // TODO: make sure that client doesn't get refresh token
+            res.send(token).status(200);
+        })
+        .catch(err => {
+            res.status(500).send(err.message);
+        });
+});
 
-function refresh(refreshToken, clientId, clientSecret) {
-    const authToken =  Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+app.get('api/refresh/:email', (req, res) => {
+    res.status(501);
 
-    return client.post('oauth/token',
+    /* TODO: to implement refresh method, refresh token has to be persisted
+    const refreshToken = ???;
+    const appIdPlusAppSecret =  Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+
+    return authApiClient.post('oauth/token',
         qs.stringify({ 
             grant_type:'refresh_token',
             refresh_token: refreshToken
@@ -105,93 +64,29 @@ function refresh(refreshToken, clientId, clientSecret) {
         {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                "Authorization": `Basic ${authToken}`
+                "Authorization": `Basic ${appIdPlusAppSecret}`
             }
         })
-        .then(response => response.data)
-        .catch(handleErrorI);
-}
+        .then(response => res.send(response.data).status(200))
+        .catch(err => res.status(500).send(err.message)); */
+ });
 
-// app.get('/refresh/:email', (req, res) => {
-//     const token =  storage[req.params.email];
+// host react app in express
+app.use(express.static(path.join(__dirname, '../build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
-//     client.oauth.refresh(token.refresh_token, config.appId, config.appSecret)
-//         .then(newToken => {
-//             storage[token.user_email] = newToken;
-
-//             res.redirect(`/app/${token.user_email}`);
-//         })
-//         .catch(err => handleError(res, err));
-// });
-
-// app.get('/app_old', (req, res) => {
-//     res.send(req.query);
-// });
-
-// app.get('/syncpoints/:email', (req, res) => {
-//     const token =  storage[req.params.email];
-
-//     const accessToken =  token.access_token;
-
-//     client.syncp.getSyncpoints(accessToken)
-//         .then(syncpoints => {
-//             res.status(200).json(syncpoints.map((sp, i, arr) => {
-//                 return {id: sp.Id, name: sp.Name, rootFolderId: sp.RootFolderId};
-//             }));
-//         })
-//         .catch(err => handleError(res, err));
-// });
-
-// app.get('/folders/:email/:syncpointId/:folderId', (req, res) => {
-//     const token = storage[req.params.email];
-
-//     const accessToken =  token.access_token;
-
-//     client.syncp.getFolders(accessToken, req.params.syncpointId, req.params.folderId)
-//         .then(syncpoints => {
-//             res.status(200).send(syncpoints.map((f, i, arr) => {return {id: f.FolderId, name: f.Name};}));
-//         })
-//         .catch(err => handleError(res, err));
-// });
-
-// app.get('/files/:email/:syncpointId/:folderId', (req, res) => {
-//     const token = storage[req.params.email];
-
-//     const accessToken =  token.access_token;
-
-//     client.syncp.getFiles(accessToken, req.params.syncpointId, req.params.folderId)
-//     .then(syncpoints => {
-//         res.status(200).send(syncpoints.map((f, i, arr) => {return {id: f.FileId, name: f.Filename};}));
-//     })
-//     .catch(err => handleError(res, err));
-// });
-
-// app.get('/link/:email', (req, res) => {
-
-//     res.send(req.params.email);
-//  });
-
-// app.post('/link/:email', (req, res) => {
-//     const token = storage[req.params.email];
-//     const data = req.body;
-
-//     const accessToken =  token.access_token;
-
-//     client.syncp.createLink(accessToken, data.syncpointId, data.virtualPath)
-//     .then(links => {
-//         res.status(200).send(links.map((l, i, a) => {
-//             return { 
-//                 landingUrl: l.LandingPageUrl,
-//                 downlaodUrl: l.DownloadUrl
-//             };
-//         }));
-//     })
-//     .catch(err => handleError(res, err));
-// });
+// handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 // error handling
 app.use((err, req, res, next) => {
     res.status(500).send(err.message);
 });
 
-module.exports = app;
+// start the server
+const port = config.port + (process.env.NODE_ENV != "production" ?  1 : 0);
+app.listen (port, () => console.log (`Started at ${port} on http`));
--- server/config.js
+++ server/config.js
@@ -3,6 +3,5 @@ require('dotenv').config();
 module.exports = {
     port: Number.parseInt(process.env.PORT),
     appId: process.env.APP_ID,
-    appSecret: process.env.APP_SECRET,
-    useHttps: process.env.USE_HTTPS === 'true'
+    appSecret: process.env.APP_SECRET
 };
